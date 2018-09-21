8923857efae36cf5c080992a09ffc1b6e41ea0d5
Ashviniv <ashvini@joshsoftware.com>
2018-09-21 09:08:38 +0530

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(email: "test@joshsoftware.com", password: "josh123")
+user2 = User.create!(email: "test1@joshsoftware.com", password: "josh123")
+user3 = User.create!(email: "test2@joshsoftware.com", password: "josh123")
+
+Mail.create!(subject: "React Workshop", to: user2.email, status: "sent", body: "Test Body", user: user1)
+Mail.create!(subject: "React Workshop", from: user1.email, status: "inbox", body: "Test Body", user: user2)
